9321af946715b0228f7fefbea50c3eb859168520
eks5115 <eks5115@139.com>
2023-06-29 17:40:12 +0800

    feat: add create

---
+++ src/exec.ts
@@ -0,0 +1,27 @@
+import { spawn } from 'child_process'
+
+interface Option {
+  cwd: string
+}
+
+let cwd = ''
+export const exec = (command: string, option?: Option): Promise<void> => {
+  if (option?.cwd) cwd = option.cwd
+  return new Promise((resolve, reject) => {
+    const commands = command.split(' ')
+    let args: string[] = []
+    if (commands.length > 1) {
+      args = commands.slice(1, commands.length)
+    }
+    const child = spawn(commands[0], args, {
+      cwd,
+      stdio: 'inherit'
+    })
+    child.on('close', (code: number) => {
+      if (code > 0) {
+        reject()
+      }
+      resolve()
+    })
+  })
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
-console.log('123')
+import { program } from 'commander'
+import { exec } from './exec'
+import path from 'path'
+
+program
+  .argument('name')
+  .action(async (name) => {
+    // crate project dir
+    await exec(`mkdir -p ${name}`)
+    await exec(`cp -a ${path.resolve(__dirname, '../template')}/ ./`, {
+      cwd: name
+    })
+    await exec('git init')
+    await exec('yarn init -y')
+    await exec('yarn add --dev @typescript-eslint/parser @typescript-eslint/eslint-plugin eslint typescript')
+  })
+program.parse()
