9ec67ec2ea50261c83b6a353008057a4eced5c9a
eks5115 <eks5115@139.com>
2026-04-13 22:03:46 +0800

    feat: add tsx and remove ts-node

--- src/template.ts
+++ src/template.ts
@@ -9,7 +9,7 @@ const getTemplatePath = () => {
 }
 
 export const addTypescript = async (context: Context) => {
-  await exec('pnpm add -D typescript ts-node')
+  await exec('pnpm add -D typescript tsx')
   await exec(`cp -a ${path.join(getTemplatePath(), 'typescript')}/ ./`)
   context.package.scripts['build'] = 'pnpm clean && tsc'
 }
@@ -26,19 +26,24 @@ export const addJest = async (context: Context) => {
   context.package.scripts['test'] = 'jest'
 }
 
-export const addExpress = async (context: Context) => {
-  await exec('pnpm add express tsconfig-paths dotenv')
-  await exec('pnpm add -D ts-node-dev @types/express')
-  await exec(`cp -a ${path.join(getTemplatePath(), 'express')}/ ./`)
-  context.package.scripts['dev'] = 'ts-node-dev --respawn --transpile-only -r tsconfig-paths/register src/app.ts'
-  context.package.scripts['serve'] = 'TS_NODE_BASEURL=./dist node -r tsconfig-paths/register dist/app.js'
+export const addLib = async (context: Context) => {
+  await exec(`cp -a ${path.join(getTemplatePath(), 'lib')}/ ./`)
+  context.package.scripts['start'] = 'tsx src/index.ts'
+  context.package.scripts['prepublishOnly'] = 'pnpm lint && pnpm build'
 }
 
 export const addFastify = async (context: Context) => {
   await exec('pnpm add fastify tsconfig-paths dotenv pino-pretty')
-  await exec('pnpm add -D ts-node-dev')
   await exec(`cp -a ${path.join(getTemplatePath(), 'fastify')}/ ./`)
-  context.package.scripts['dev'] = 'ts-node-dev --respawn --transpile-only -r tsconfig-paths/register src/app.ts'
+  context.package.scripts['dev'] = 'tsx watch src/app.ts'
+  context.package.scripts['serve'] = 'TS_NODE_BASEURL=./dist node -r tsconfig-paths/register dist/app.js'
+}
+
+export const addExpress = async (context: Context) => {
+  await exec('pnpm add express tsconfig-paths dotenv')
+  await exec('pnpm add -D @types/express')
+  await exec(`cp -a ${path.join(getTemplatePath(), 'express')}/ ./`)
+  context.package.scripts['dev'] = 'tsx watch src/app.ts'
   context.package.scripts['serve'] = 'TS_NODE_BASEURL=./dist node -r tsconfig-paths/register dist/app.js'
 }
 
@@ -62,7 +67,7 @@ export const libTemplate = async (context: Context) => {
   await addTypescript(context)
   await addEslint(context)
   await addJest(context)
-  context.package.scripts['prepublishOnly'] = 'pnpm lint && pnpm build'
+  await addLib(context)
 }
 
 export const expressTemplate = async (context: Context) => {
